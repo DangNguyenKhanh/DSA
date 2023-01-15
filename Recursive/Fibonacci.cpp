7b3ba4ce8609bb3082be8c91582014bfd2b24bbd
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-15 16:54:39 +0700

    Create Fibonacci.cpp

---
+++ Recursive/Fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+// 1 1 2 3 5 8
+// 0 1 2 3 4 5: index
+unsigned long long Fibonacci(int index)
+{
+    
+    if(index == 0 || index == 1) return 1;
+    return Fibonacci(index - 1) + Fibonacci(index - 2);
+}
+
+int main()
+{
+    cout << Fibonacci(4) << endl;
+}
