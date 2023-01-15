84675612820ac9c539c7674767a0a4803b16f9c9
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-15 14:46:28 +0700

    Create GCD.cpp

---
+++ Basic-Math/GCD.cpp
@@ -0,0 +1,23 @@
+Greatest Common Divisor - ước chung lớn nhất
+
+#include<iostream>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    while(b != 0)
+    {
+        if(a > b)
+            a = a - b;
+        else   
+            b = b - a;
+    }
+    return a;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << GCD(a, b) << endl;
+}
