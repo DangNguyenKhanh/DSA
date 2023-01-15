79a92e2538eea88f620b32f17c67fb967203f120
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-16 00:00:12 +0700

    Update Binary.cpp

--- Searching/Binary.cpp
+++ Searching/Binary.cpp
@@ -1,3 +1,36 @@
 TÌM KIẾM NHỊ PHÂN
+- Bắt buộc mảng phải được sắp xếp trước
+- Time complexity = O(log2|n)
+  
 
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int a[], int n, int key)
+{
+    int index = -1;
+    int i = 0;
+    int j = n - 1;
+    while(i <= j)
+    {
+        int k = (i + j) / 2;
+        if(key < a[k])
+            j = k - 1;
+        else if(key == a[k])
+        {
+            index = k;
+            break;
+        }
+        else
+            i = k + 1;
+    }
+    return index;
+}
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5, 10, 12};
+    int n = sizeof(a) / sizeof(int);
+    cout << BinarySearch(a, n, 5) << endl;
+}
 
