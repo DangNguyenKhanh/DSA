a578406297761da8741d723ff2cded91e1766404
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-16 10:42:11 +0700

    Update Queue.cpp

--- Data-structure/Restricted list/Queue.cpp
+++ Data-structure/Restricted list/Queue.cpp
@@ -2,114 +2,145 @@
 using namespace std;
 
 template<class T>
+class Node {
+public:
+	T Info;
+	Node<T>* Next;
+public:
+	Node(T);
+	void Display();
+};
+
+template<class T>
+Node<T>::Node(T Data)
+{
+	this->Info = Data;
+	this->Next = NULL;
+}
+
+template<class T>
+void Node<T>::Display()
+{
+	cout << Info << endl;
+}
+
+template<class T>
 class Queue {
-private:
-	template<class T>
-	class Node;
 private:
 	Node<T>* Front;
 	Node<T>* Rear;
 	int Count;
 public:
-	Queue() : Front(NULL), Rear(NULL), Count(0) {};
-
-	bool IsEmpty()
-	{
-		return this->Front == NULL;
-	}
-
-	void Enqueue(T data)
-	{
-		Node<T>* p = new Node<T>(data);
-		if (this->IsEmpty())
-		{
-			this->Front = p;
-		}
-		else
-		{
-			this->Rear->Next = p;
-		}
-		this->Rear = p;
-		this->Count++;
-	}
-
-	T Dequeue()
-	{
-		T result;
-		if (IsEmpty()) result = 0;
-		else
-		{
-			Node<T>* p = this->Front;
-			result = p->Info;
-			if (this->Front == this->Rear)
-			{
-				this->Rear = NULL;
-			}
-			this->Front = p->Next;
-			delete p;
-			this->Count--;
-		}
-		return result;
-	}
-
-	T Peek()
-	{
-		T result;
-		if (IsEmpty()) result = 0;
-		else
-		{
-			result = this->Front->Info;
-		}
-		return result;
-	}
-
-	void Display()
-	{
-		if (IsEmpty())
-		{
-			cout << "Hang doi rong ..." << endl;
-		}
-		else
-		{
-			cout << "Noi dung hang doi: " << endl;
-			Node<T>* p = this->Front;
-			while (p != NULL)
-			{
-				p->Display();
-				p = p->Next;
-			}
-			cout << "So luong phan tu: " << this->Count << endl;
-		}
-	}
-
-	void Clear()
-	{
-		Node<T>* p;
-		while (this->Front != NULL)
-		{
-			p = this->Front;
-			this->Front = p->Next;
-			delete p;
-		}
-		Rear = NULL;
-		this->Count = 0;
-	}
-
-	~Queue() { Clear(); }
-
-private:
-	template<class T>
-	class Node {
-	public:
-		T Info;
-		Node<T>* Next;
-	public:
-		Node(T data) : Info(data), Next(NULL) {};
-		void Display()
-		{
-			cout << Info << endl;
-		}
-	};
+	Queue();
+	bool IsEmpty();
+	void Enqueue(T);
+	T Dequeue();
+	T Peek();
+	void Display();
+	void Clear();
+	~Queue();
 };
 
 template<class T>
+Queue<T>::Queue()
+{
+	this->Front = NULL;
+	this->Rear = NULL;
+	this->Count = 0;
+}
+
+template<class T>
+bool Queue<T>::IsEmpty()
+{
+	return this->Front == NULL;
+}
+
+template<class T>
+void Queue<T>::Enqueue(T Data)
+{
+	Node<T>* p = new Node<T>(Data);
+	if (this->IsEmpty())
+	{
+		this->Front = p;
+	}
+	else
+	{
+		this->Rear->Next = p;
+	}
+	this->Rear = p;
+	this->Count++;
+}
+
+template<class T>
+T Queue<T>::Dequeue()
+{
+	T result;
+	if (this->IsEmpty()) 
+		result = 0;
+	else
+	{
+		Node<T>* p = this->Front;
+		result = p->Info;
+		if (this->Front == this->Rear)
+		{
+			this->Rear = NULL;
+		}
+		this->Front = p->Next;
+		delete p;
+		this->Count--;
+	}
+	return result;
+}
+
+template<class T>
+T Queue<T>::Peek()
+{
+	T result;
+	if (this->IsEmpty()) 
+		result = 0;
+	else
+	{
+		result = this->Front->Info;
+	}
+	return result;
+}
+
+template<class T>
+void Queue<T>::Display()
+{
+	if (this->IsEmpty())
+	{
+		cout << "Hang doi rong ..." << endl;
+	}
+	else
+	{
+		cout << "Noi dung hang doi: " << endl;
+		Node<T>* p = this->Front;
+		while (p != NULL)
+		{
+			p->Display();
+			p = p->Next;
+		}
+		cout << "So luong phan tu: " << this->Count << endl;
+	}
+}
+
+template<class T>
+void Queue<T>::Clear()
+{
+	Node<T>* p;
+	while (this->Front != NULL)
+	{
+		p = this->Front;
+		this->Front = p->Next;
+		delete p;
+	}
+	Rear = NULL;
+	this->Count = 0;
+}
+
+template<class T>
+Queue<T>::~Queue()
+{
+	this->Clear();
+}
