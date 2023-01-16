e644adc600a2bc13b6c865b4d2353d8c0c4ba941
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-16 10:22:24 +0700

    Update Stack.cpp

--- Data-structure/Restricted list/Stack.cpp
+++ Data-structure/Restricted list/Stack.cpp
@@ -7,82 +7,123 @@ public:
 	T Info;
 	Node<T>* Next;
 public:
-	Node(T data) : Info(data), Next(NULL) {};
-	void Display()
-	{
-		cout << this->Info << endl;
-	}
+	Node(T);
+	void Display();
 };
+
+template<class T>
+Node<T>::Node(T Data)
+{
+	this->Info = Data;
+	this->Next = NULL;
+}
+
+template<class T>
+void Node<T>::Display()
+{
+	cout << this->Info << endl;
+}
 
 template<class T>
 class Stack {
 private:
-	Node<T>* sp;
-	int count;
+	Node<T>* Sp;
+	int Count;
 public:
-	Stack() : sp(NULL), count(0) {};
-	bool IsEmpty()
-	{
-		return sp == NULL;
-	}
-	void Push(T data)
-	{
-		Node<T>* p = new Node<T>(data);
-		p->Next = sp;
-		sp = p;
-		this->count++;
-	}
-	T Pop()
-	{
-		T result;
-		if (IsEmpty())
-			result = 0;
-		else
-		{
-			Node<T>* p = sp;
-			result = p->Info;
-			sp = p->Next;
-			delete p;
-			this->count--;
-		}
-		return result;
-	}
-	T Peek()
-	{
-		T result;
-		if (this->IsEmpty()) result = 0;
-		else
-		{
-			result = sp->Info;
-		}
-		return result;
-	}
-	void Display()
-	{
-		if (this->IsEmpty())
-			cout << "Stack rong ..." << endl;
-		else
-		{
-			cout << "Noi dung cua Stack: " << endl;
-			Node<T>* p = sp;
-			while (p != NULL)
-			{
-				p->Display();
-				p = p->Next;
-			}
-			cout << "So phan tu cua Stack: " << this->count << endl;
-		}
-	}
-	void Clear()
-	{
-		Node<T>* p;
-		while (sp != NULL)
-		{
-			p = sp;
-			sp = p->Next;
-			delete p;
-		}
-		this->count = 0;
-	}
-	~Stack() { this->Clear(); };
+	Stack();
+	bool IsEmpty();
+	void Push(T);
+	T Pop();
+	T Peek();
+	void Display();
+	void Clear();
+	~Stack();
 };
+
+template<class T>
+Stack<T>::Stack()
+{
+	this->Sp = NULL;
+	this->Count = 0;
+}
+
+template<class T>
+bool Stack<T>::IsEmpty()
+{
+	return Sp == NULL;
+}
+
+template<class T>
+void Stack<T>::Push(T Data)
+{
+	Node<T>* p = new Node<T>(Data);
+	p->Next = Sp;
+	Sp = p;
+	this->Count++;
+}
+
+template<class T>
+T Stack<T>::Pop()
+{
+	T result;
+	if (IsEmpty())
+		result = 0;
+	else
+	{
+		Node<T>* p = Sp;
+		result = p->Info;
+		Sp = p->Next;
+		delete p;
+		this->Count--;
+	}
+	return result;
+}
+
+template<class T>
+T Stack<T>::Peek()
+{
+	T result;
+	if (this->IsEmpty()) 
+		result = 0;
+	else
+		result = Sp->Info;
+	return result;
+}
+
+template<class T>
+void Stack<T>::Display()
+{
+	if (this->IsEmpty())
+		cout << "Stack rong ..." << endl;
+	else
+	{
+		cout << "Noi dung cua Stack: " << endl;
+		Node<T>* p = Sp;
+		while (p != NULL)
+		{
+			p->Display();
+			p = p->Next;
+		}
+		cout << "So phan tu cua Stack: " << this->Count << endl;
+	}	
+}
+
+template<class T>
+void Stack<T>::Clear()
+{
+	Node<T>* p;
+	while (Sp != NULL)
+	{
+		p = Sp;
+		Sp = p->Next;
+		delete p;
+	}
+	this->Count = 0;
+}
+
+template<class T>
+Stack<T>::~Stack()
+{
+	this->Clear();
+}
+
